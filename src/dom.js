d56a3421323bf76649dc3960b220b98e862410bd
Mupa1 <mupasmail@gmail.com>
2020-10-16 19:24:31 +0200

    add logic to toggle between celcius and fahrenheit

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,7 @@
 const searchbox = document.querySelector('.search-box');
 const city = document.querySelector('.city');
 const temp = document.querySelector('.temp');
+const btns = document.querySelector('.btn-group');
 
 const domElements = (() => {
   const showCity = (res) => {
@@ -11,13 +12,19 @@ const domElements = (() => {
     temp.innerHTML = `${Math.round(res.main.temp)}<span>°C</span>`;
   };
 
+  const showTempF = (res) => {
+    temp.innerHTML = `${Math.round((res.main.temp * 1.8) + 32)}<span>°F</span>`;
+  };
+
   return {
     showCity,
     showTempC,
+    showTempF,
   };
 })();
 
 export {
   searchbox,
   domElements,
+  btns,
 };
--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ import './styles.css';
 import {
   searchbox,
   domElements,
+  btns,
 } from './dom';
 
 const key = 'e0fbc2a16f333c42771b462f6a2ea420';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/';
 
+const switchTemps = (res) => {
+  btns.addEventListener('click', event => {
+    if (event.target.classList.contains('celcius')) {
+      domElements.showTempC(res);
+    } else if (event.target.classList.contains('fahrenheit')) {
+      domElements.showTempF(res);
+    }
+  });
+};
+
 const displayResults = (weather) => {
   domElements.showCity(weather);
   domElements.showTempC(weather);
+  switchTemps(weather);
 };
 
 const fetchResults = (city) => {
